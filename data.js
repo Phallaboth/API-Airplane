cff5c294658171fae3a0e471a959ec497548af25
Phallaboth <beflexter@gmail.com>
2025-10-20 23:42:12 +0700

    update code

--- data.js
+++ data.js
@@ -3,19 +3,16 @@ fetch("https://phallaboth.github.io/API-Airplane/data.json")
 .then(response => response.json())
 .then(item => {
     item.forEach(i => {
-        show += `
-        
-            <div class="col-lg-3 d-flex justify-content-center">
-                <div class="card" style="width: 18rem; height:500px">
-                    <img src="${i.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${i.name}</h5>
-                        <p class="card-text">${i.description}</p>
-                        <p class="card-text">${i.price}</p>
-                        <a href="#" class="btn btn-primary">Add to cart</a>
-                    </div>
-                </div>
-            </div>
+        show += `                   
+                    <div class="card py-5" style="width: 18rem; height:90vh">
+                        <img src="${i.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${i.name}</h5>
+                            <p class="card-text">${i.description}</p>
+                            <p class="card-text">${i.price}</p>
+                            <a href="#" class="btn btn-primary">Add to cart</a>
+                        </div>
+                    </div>           
         `
         
         document.getElementById("output").innerHTML = show
