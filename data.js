af91b7bef9f7b1c2a2196de33e569c7ef666fee3
Phallaboth <beflexter@gmail.com>
2025-10-20 19:04:02 +0700

    update

---
+++ data.js
@@ -0,0 +1,27 @@
+let show =``
+fetch("https://phallaboth.github.io/API-Airplane/data.json")
+.then(response => response.json())
+.then(item => {
+    item.forEach(i => {
+        show += `
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem; height:500px">
+                    <img src="${i.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${i.name}</h5>
+                        <p class="card-text">${i.description}</p>
+                        <p class="card-text">${i.price}</p>
+                        <a href="#" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>       
+        `
+        
+        document.getElementById("output").innerHTML = show
+    })
+})
+
+.catch(err => console.log(`Error API ${err}`));
